b625048151bbd1b46a2818625a7fd99fb659ee49
Nenye <57047007+nenye-a@users.noreply.github.com>
2024-07-27 14:11:09 -0500

    add prettier and eslint

---
+++ backend/src/.eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  env: {
+    node: true,
+    // es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: ".",
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint", "graphql"],
+  rules: {
+    // Custom rules
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "prettier/prettier": "error",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".ts", ".tsx"],
+      },
+      typescript: {},
+    },
+  },
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      rules: {
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+      },
+    },
+    {
+      files: ["**/*.graphql"],
+      parser: "eslint-plugin-graphql",
+      plugins: ["graphql"],
+      // rules: {
+      //   'graphql/template-strings': [
+      //     'error',
+      //     {
+      //       env: 'literal',
+      //       schemaJson: require('./path/to/your/schema.json'), // Update with the correct path to your schema.json
+      //     },
+      //   ],
+      // },
+    },
+  ],
+};
