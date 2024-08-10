f9dacdecb6de210c39d31fff411fa2d10ba990ff
Nenye <57047007+nenye-a@users.noreply.github.com>
2024-08-10 17:56:16 -0500

    link to the documentation

--- backend/src/api/google/index.ts
+++ backend/src/api/google/index.ts
@@ -1,2 +1,7 @@
+/**
+ * Custom support for the Google API:
+ * @see https://developers.google.com/maps/documentation/places/web-service/op-overview
+ */
+
 export * as google from './google';
 export * from './types';
