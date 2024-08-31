93c4411fc87b2b2d909434fcfabed4bd4c526aef
Nenye <57047007+nenye-a@users.noreply.github.com>
2024-08-31 17:20:57 -0500

    add support for geolocation

--- frontend/src/components/atoms/AfterMap/AfterMap.tsx
+++ frontend/src/components/atoms/AfterMap/AfterMap.tsx
@@ -1,27 +1,36 @@
 // import { useMemo } from 'react';
+import { PropsWithChildren } from 'react';
 import MapView, { MapViewProps, Region } from 'react-native-maps';
 
-type Props = MapViewProps & {
-  // TODO: Add custom after map props.
+// San francisco coordinates is the default region.
+const defaultRegion: Region = {
+  latitude: 37.78825,
+  longitude: -122.4324,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
 };
 
-function AfterMap({ ...props }: Props) {
-  let testRegion: Region = {
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+type Props = MapViewProps &
+  PropsWithChildren & {
+    // TODO: Add custom after map props.
   };
 
+function AfterMap({
+  children,
+  initialRegion = defaultRegion,
+  ...props
+}: Props) {
   return (
     <MapView
       style={{
         height: 1000,
         width: 400,
       }}
-      initialRegion={testRegion}
+      initialRegion={initialRegion}
       {...props}
-    ></MapView>
+    >
+      {children}
+    </MapView>
   );
 }
 
